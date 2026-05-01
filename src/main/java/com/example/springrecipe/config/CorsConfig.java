f9010a9b738f4a579d3f0271625ab3ff3f25f897
qwerty123tik <naastalihimovic@gmail.com>
2026-05-01 22:15:27 +0300

    fixx

--- src/main/java/com/example/springrecipe/config/CorsConfig.java
+++ src/main/java/com/example/springrecipe/config/CorsConfig.java
@@ -9,7 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://javalab1-production.up.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
